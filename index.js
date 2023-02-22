9e43cd000348ab1ed4f4e2134020e68c054cd80f
Benjamin <wagner.benjamin99@gmail.com>
2023-02-21 23:12:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 
-const xhr = new XMLHttpRequest();
+initSqlJs({ locateFile: filename => `https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.8.0/sql-wasm.js` }).then(function(SQL)
+{
+  const xhr = new XMLHttpRequest();
 
-xhr.open('GET', 'http://www.amusicalcoder.com/geLocator.github.io/ge_locations.db', true);
-xhr.responseType = 'arraybuffer';
+  xhr.open('GET', 'http://www.amusicalcoder.com/geLocator.github.io/ge_locations.db', true);
+  xhr.responseType = 'arraybuffer';
 
-xhr.onload = e => {
-  const uInt8Array = new Uint8Array(xhr.response);
-  const db = new SQL.Database(uInt8Array);
-  const contents = db.exec("SELECT * FROM my_table");
-  
-};
-xhr.send();
+  xhr.onload = e => {
+    const uInt8Array = new Uint8Array(xhr.response);
+    const db = new SQL.Database(uInt8Array);
+    const contents = db.exec("SELECT * FROM my_table");
+    
+  };
+  xhr.send();
+
+});
