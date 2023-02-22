2b37f4e93f5a686d9bba52c92856928917d1a6df
Benjamin <wagner.benjamin99@gmail.com>
2023-02-22 10:20:33 -0500

    Update index.js
    Should be completely functional. Pending testing.

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-var contents;
 
 window.onload = (event) => 
 {
-  
   initSqlJs({ locateFile: filename => 'http://www.amusicalcoder.com/geLocator.github.io/' + filename }).then(function(SQL)
   {
     const xhr = new XMLHttpRequest();
@@ -13,17 +11,23 @@ window.onload = (event) =>
     xhr.onload = e => {
       const uInt8Array = new Uint8Array(xhr.response);
       const db = new SQL.Database(uInt8Array);
-      this.contents = db.exec("SELECT * FROM locations");
-      console.log(this.contents);
-      
+      const contents = db.exec("SELECT * FROM locations");
+
+      const data = new Map();
+
+      for(let i = 0; i < contents[0].values.length; i++)
+      {
+        data.set(contents[0].values[i][0], contents[0].values[i][1]);
+      }
+
+      button = document.getElementById("enter_button");
+
+      button.addEventListener('click', (event) => {
+        document.getElementById("addressText").innerHTML = data.get(document.getElementById("storeNumText").value);
+      });
     };
     xhr.send();
 
   });
 
-  button = document.getElementById("enter_button");
-
-  button.addEventListener('click', function(event) {
-    document.getElementById("addressText").innerHTML = this.contents[document.getElementById("storeLabel").value]; //WHERE WE LEFT OFF. understand layout of contents to get data out
-  });  
 }
